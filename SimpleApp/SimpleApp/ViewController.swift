3678bfb9f1aa52e2e0e3d455f51a98e0104a48cd
minjae-L <54211648+minjae-L@users.noreply.github.com>
2020-06-29 10:52:05 +0900

    simple App
    for autolayout

---
+++ SimpleApp/SimpleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleApp
+//
+//  Created by 이민재 on 29/06/2020.
+//  Copyright © 2020 이민재. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
